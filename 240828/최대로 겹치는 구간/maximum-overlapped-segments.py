9bd4ca598d5b33dbcae87f5aeb8fb01f1dd40cdb
Jeongwoo Lee <jwlee87@yonsei.ac.kr>
2024-08-28 01:43:11 +0900

    Title: 최대로 겹치는 구간, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240828/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,10 +1,13 @@
 n = int(input())
-arr = [0 for _ in range(300)]
+# 겹침을 처리할 배열 0으로 초기화
+arr = [0] * 300 # [0 for _ in range(300)]
+# 음수 값 처리를 위해 오프셋
 offset = 100
 
 for i in range(n):
     x1, x2 = map(int, input().split())
     
+    # 오프셋을 적용하여 음수 --> 양수 
     x1 += offset
     x2 += offset
 
