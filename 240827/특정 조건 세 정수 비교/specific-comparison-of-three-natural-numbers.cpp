f32d2cd5fa44800337b2b75e929ab95135b4de00
Jeongwoo Lee <jwlee87@yonsei.ac.kr>
2024-08-27 17:37:00 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240827/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int min_value;
+    min_value = min({a, b, c});
+    cout << (a == min_value) << " " << (a == b && b == c && a == c);
+
+    return 0;
+}
